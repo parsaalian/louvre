cafc4fce9cdef81f680518e57280ebebfa34802a
parsa alian <parsaalian0@gmail.com>
2020-08-01 09:51:30 +0430

    changed main page navbar

--- app/src/components/Home/home.js
+++ app/src/components/Home/home.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Button } from "rsuite";
 
 const Home = (props) => {
     return (
-        <div className="logout_container">
-            <h1>به پروژه درس بلاک چین خوش آمدید</h1>
+        <div className="logout_container" style={{ textAlign: "center" }}>
+            <h1>به بازی لورر خوش آمدید</h1>
+            <a href="login" style={{ margin: "1rem" }}>
+                <Button appearance="primary">ورود</Button>
+            </a>
+            <a href="register" style={{ margin: "1rem" }}>
+                <Button appearance="primary">ثبت‌نام</Button>
+            </a>
         </div>
     );
 };
--- app/src/hoc/layout.js
+++ app/src/hoc/layout.js
@@ -1,50 +1,62 @@
 import React from "react";
+import { useRouteMatch } from "react-router-dom";
 import { Navbar, Nav, Dropdown, Icon } from "rsuite";
 // import Header from '../components/Header/header';
 
 const Layout = ({ children }) => {
+    const match = useRouteMatch();
+    console.log(match);
+
     return (
         <div style={{ direction: "rtl" }}>
-            <Navbar appearance="inverse">
-                <Navbar.Body>
-                    <Nav>
-                        <Nav.Item icon={<Icon icon="home" />} href="/dashboard">
-                            داشبورد
-                        </Nav.Item>
-                        <Nav.Item
-                            icon={<Icon icon="paint-brush" />}
-                            href="createPainting"
-                        >
-                            نقاشی جدید
-                        </Nav.Item>
-                        <Nav.Item icon={<Icon icon="image" />} href="/gallery">
-                            آثار
-                        </Nav.Item>
-                        <Nav.Item
-                            icon={<Icon icon="money" />}
-                            href="/chargeAccount"
-                        >
-                            شارژ حساب
-                        </Nav.Item>
-                    </Nav>
-                    <Nav pullRight>
-                        <Dropdown title="تنظیمات">
-                            <Dropdown.Item
-                                icon={<Icon icon="cog" />}
-                                href="changePassword"
+            {!["/", "/login", "register"].includes(match.path) && (
+                <Navbar appearance="inverse">
+                    <Navbar.Body>
+                        <Nav>
+                            <Nav.Item
+                                icon={<Icon icon="home" />}
+                                href="/dashboard"
                             >
-                                تغییر رمز
-                            </Dropdown.Item>
-                            <Dropdown.Item
-                                icon={<Icon icon="sign-out" />}
-                                href="/logout"
+                                داشبورد
+                            </Nav.Item>
+                            <Nav.Item
+                                icon={<Icon icon="paint-brush" />}
+                                href="createPainting"
                             >
-                                خروج
-                            </Dropdown.Item>
-                        </Dropdown>
-                    </Nav>
-                </Navbar.Body>
-            </Navbar>
+                                نقاشی جدید
+                            </Nav.Item>
+                            <Nav.Item
+                                icon={<Icon icon="image" />}
+                                href="/gallery"
+                            >
+                                آثار
+                            </Nav.Item>
+                            <Nav.Item
+                                icon={<Icon icon="money" />}
+                                href="/chargeAccount"
+                            >
+                                شارژ حساب
+                            </Nav.Item>
+                        </Nav>
+                        <Nav pullRight>
+                            <Dropdown title="تنظیمات">
+                                <Dropdown.Item
+                                    icon={<Icon icon="cog" />}
+                                    href="changePassword"
+                                >
+                                    تغییر رمز
+                                </Dropdown.Item>
+                                <Dropdown.Item
+                                    icon={<Icon icon="sign-out" />}
+                                    href="/logout"
+                                >
+                                    خروج
+                                </Dropdown.Item>
+                            </Dropdown>
+                        </Nav>
+                    </Navbar.Body>
+                </Navbar>
+            )}
             {children}
         </div>
     );
